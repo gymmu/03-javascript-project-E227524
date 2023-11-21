aec1172baf223fe897950ff03db150628cfaf949
Max Brügger <e227524@edu.sbl.ch>
2023-11-21 10:07:39 +0100

    Aufgabe 1,2,3 & 4

--- script.js
+++ script.js
@@ -4,7 +4,52 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if(currentElement === "e" || currentElement === "E") {
+
+    }
+    else {
     result.push(currentElement)
+    }
   }
   return result.join("")
+}
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    let currentElement = input[i]
+    currentElement = currentElement.toUpperCase()
+    result.push(currentElement) 
+  }
+  return result.join("")
+}
+export function aufgabe03 (args) {
+  const input = args
+  const result = []
+
+  let countE = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if(currentElement === "e" || currentElement === "E") {
+      countE++
+    }
+  }
+  return countE
+}
+export function aufgabe04 (args) {
+  let input = args
+  const result = []
+
+  let countW = 1
+
+  input = input.replace(/\s\s+/g, ' ')
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === " ") {
+    countW++
+    }
+  }
+  return countW
 }
